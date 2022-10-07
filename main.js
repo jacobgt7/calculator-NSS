9b44473adf5b61050b0cd0d007bcd7bfab30863a
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 09:23:26 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+const subtract = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber
+}
+
+const divide = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber
+}
+
+const square = (num) => {
+    return num * num
+}
+
+
+let leg4 = multiply(4, 0.25)
+console.log(leg4)
+
+let leg2 = multiply(0.75, 2)
+console.log(leg2)
+
+let addAnimals = add(1, 1.5)
+console.log(addAnimals)
+
+let divAnimals = divide(60, 2.5)
+console.log(divAnimals)
